274a9221efea543bd888eeeee8717ac6347756ec
Liberman, Dor <dor.liberman@intel.com>
2025-06-17 21:21:07 +0300

    add fresh hw2

---
+++ hw_2_fresh/build/CMakeFiles/tanks_game.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/algo/MyBattleStatus.cpp" "CMakeFiles/tanks_game.dir/src/algo/MyBattleStatus.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/algo/MyBattleStatus.cpp.o.d"
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/algo/MyPlayerFactory.cpp" "CMakeFiles/tanks_game.dir/src/algo/MyPlayerFactory.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/algo/MyPlayerFactory.cpp.o.d"
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/algo/MyTankAlgorithm.cpp" "CMakeFiles/tanks_game.dir/src/algo/MyTankAlgorithm.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/algo/MyTankAlgorithm.cpp.o.d"
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/algo/MyTankAlgorithmFactory.cpp" "CMakeFiles/tanks_game.dir/src/algo/MyTankAlgorithmFactory.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/algo/MyTankAlgorithmFactory.cpp.o.d"
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/algo/PathfindingAlgorithm.cpp" "CMakeFiles/tanks_game.dir/src/algo/PathfindingAlgorithm.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/algo/PathfindingAlgorithm.cpp.o.d"
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/algo/Player1.cpp" "CMakeFiles/tanks_game.dir/src/algo/Player1.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/algo/Player1.cpp.o.d"
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/algo/Player2.cpp" "CMakeFiles/tanks_game.dir/src/algo/Player2.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/algo/Player2.cpp.o.d"
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/algo/SimpleAlgorithm.cpp" "CMakeFiles/tanks_game.dir/src/algo/SimpleAlgorithm.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/algo/SimpleAlgorithm.cpp.o.d"
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/game_manager/ActionRequest.cpp" "CMakeFiles/tanks_game.dir/src/game_manager/ActionRequest.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/game_manager/ActionRequest.cpp.o.d"
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/game_manager/Board.cpp" "CMakeFiles/tanks_game.dir/src/game_manager/Board.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/game_manager/Board.cpp.o.d"
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/game_manager/Collision.cpp" "CMakeFiles/tanks_game.dir/src/game_manager/Collision.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/game_manager/Collision.cpp.o.d"
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/game_manager/GameManager.cpp" "CMakeFiles/tanks_game.dir/src/game_manager/GameManager.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/game_manager/GameManager.cpp.o.d"
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/game_manager/GameObjectFactory.cpp" "CMakeFiles/tanks_game.dir/src/game_manager/GameObjectFactory.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/game_manager/GameObjectFactory.cpp.o.d"
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/game_manager/InputParser.cpp" "CMakeFiles/tanks_game.dir/src/game_manager/InputParser.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/game_manager/InputParser.cpp.o.d"
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/game_manager/Logger.cpp" "CMakeFiles/tanks_game.dir/src/game_manager/Logger.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/game_manager/Logger.cpp.o.d"
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/game_manager/MySatelliteView.cpp" "CMakeFiles/tanks_game.dir/src/game_manager/MySatelliteView.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/game_manager/MySatelliteView.cpp.o.d"
+  "/Users/dorliber/Advanced-Concepts-in-Programming/hw_2_fresh/src/main.cpp" "CMakeFiles/tanks_game.dir/src/main.cpp.o" "gcc" "CMakeFiles/tanks_game.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
